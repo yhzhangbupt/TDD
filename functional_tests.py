af86c7dfd9136e07cb9cf4b3d64db1cb03193dc3
zyh <zhangyihua@mbpzhang>
2020-04-07 11:56:13 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -16,6 +16,33 @@ class NewVisitorTest(unittest.TestCase):
         
         #she notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)#(4)
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
+        #She is invited to enter a to-do item straight away
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        
+        #She types "Buy peacock features" into a text box(Edith`s hobby
+        #is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock features')
+        
+        #when she hits enter, the page updatas, and now the page lists
+        #"1: Buy peacock features" as am item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text=='1: Buy peacock features' for row in rows)
+            )
+
+        #There is still a text boxx inviting her to add another item. She
+        #enters :Use peacock features to make a fly:(Edith is very methodical)
         self.fail('Finish the test!' )#(5)
         
 if __name__ == '__main__':#(6)
@@ -28,16 +55,12 @@ if __name__ == '__main__':#(6)
 #assert 'Django' in browser.title
 #assert 'To-Do' in browser.title,"Browser title was" + browser.title     #(4)
 
-#She is invited to enter a to-do item straight away
 
-#She types "Buy peacock features" into a text box(Edith`s hobby
-#is tying fly-fishing lures)
 
-#when she hits enter, the page updatas, and now the page lists
-#"1: Buy peacock features" as am item in a to-do list
 
-#There is still a text boxx inviting her to add another item. She
-#enters :Use peacock features to make a fly:(Edith is very methodical)
+
+
+
 
 #The page updates again, and now shows both items on her list
 
