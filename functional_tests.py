64000d06a4c453bbc1c12745e9b078484f92010d
zyh <zhangyihua@mbpzhang>
2020-04-21 11:16:33 +0800

    ppt5 37

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()#(2)
         
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-do app. She goes
         #to check out its homepage
@@ -36,14 +42,14 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy peacock features" as am item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table=self.browser.find_element_by_id('id_list_table')
+        '''table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
         #self.assertTrue(
           #  any(row.text=='1: Buy peacock features' for row in rows),
           #  f"New to-do item did not appear in table. Contents were:\n{table.text}"
-          #  )
-        self.assertIn('1: Buy peacock features',[row.text for row in rows])
+          #  )'''
+        #self.assertIn('1: Buy peacock features',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock features')
 
         #There is still a text boxx inviting her to add another item. She
         #enters :Use peacock features to make a fly:(Edith is very methodical)
@@ -53,14 +59,16 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         #The page updates again, and now shows both items on her list
-        table=self.browser.find_element_by_id('id_list_table')
+        '''table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
         self.assertIn('1: Buy peacock features',[row.text for row in rows])
         self.assertIn(
             '2: Use peacock features to make a fly',
             [row.text for row in rows]
-            )
-            
+            )'''
+        self.check_for_row_in_list_table('1: Buy peacock features')
+        self.check_for_row_in_list_table('2: Use peacock features to make a fly')
+        
         #Edith wonders whether the site will remermber her list.Then she sees
         #that the site hae generated a unique URL for her -- there is some
         #exxplanatary text to that effect.
