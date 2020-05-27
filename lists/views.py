22a64af82b596321320d12d695f1edeacac47866
zyh <zhangyihua@mbpzhang>
2020-05-27 19:39:24 +0800

    new URL + view for adding to existing lists.FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -23,15 +23,21 @@ def home_page(request):
     #return render(request,'home.html',{
     #    'new_item_text':request.POST.get('item_text',''),
      #   })
-def view_list(request):
-    items=Item.objects.all()
-    return render(request,'list.html',{'items':items})
+def view_list(request,list_id):
+    list_=List.objects.get(id=list_id)
+    #items=Item.objects.filter(list=list_)
+    return render(request,'list.html',{'list':list_})
  
 def new_list(request):
     list_=List.objects.create()
     Item.objects.create(text=request.POST['item_text'],list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
-    
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request,list_id):
+    list_=List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'],list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
 #    return HttpResponse('<html><title>To-Do lists</title></html>')
 # Create your views here.
 #home_page=None
